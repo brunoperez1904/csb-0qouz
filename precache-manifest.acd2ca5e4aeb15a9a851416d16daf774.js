3088878e4f8c55a7034f8775d5570dabbd49e832
Code Sandbox <hello@codesandbox.io>
2021-05-05 20:28:43 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.acd2ca5e4aeb15a9a851416d16daf774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405f0b8480fe0f39dc7d5c082514739c",
+    "url": "https://brunoperez1904.github.io/csb-0qouz/index.html"
+  },
+  {
+    "revision": "faf298f3053c2669b5b2",
+    "url": "https://brunoperez1904.github.io/csb-0qouz/static/js/2.25242a11.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "https://brunoperez1904.github.io/csb-0qouz/static/js/2.25242a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336d82684a02badf0d42",
+    "url": "https://brunoperez1904.github.io/csb-0qouz/static/js/main.dc815bba.chunk.js"
+  },
+  {
+    "revision": "099242996cae20c6f06e",
+    "url": "https://brunoperez1904.github.io/csb-0qouz/static/js/runtime-main.938e685f.js"
+  }
+]);
